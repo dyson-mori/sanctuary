ab9b1ee0d8f31090002c1c2410d08a11885fae8a
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2024-12-12 22:14:42 -0300

    :boom: export all svgs

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+import { ReactComponent as Add } from './add.svg';
+import { ReactComponent as Key } from './key.svg';
+import { ReactComponent as Onlyfans } from './onlyfans.svg';
+import { ReactComponent as Search } from './search-normal.svg';
+import { ReactComponent as Twitter } from './x.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as Upload } from './upload.svg';
+
+export {
+  Add,
+  Key,
+  Onlyfans,
+  Search,
+  Twitter,
+  Logo,
+  Upload
+}
