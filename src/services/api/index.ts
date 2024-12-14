00a55ea86e4aba6d6dd4fecb3ec3e26d23ed6a0e
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2024-12-14 08:33:53 -0300

    :sparkles: add endpoint create post

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -4,10 +4,12 @@ import { ApiProps } from './types'
 
 export const api: ApiProps = {
   posts: {
-    list: () => fetcher({ url: '/post', method: 'GET' })
+    list: () => fetcher({ url: '/post', method: 'GET' }),
+    create: (body) => fetcher({ url: '/post', method: 'POST', body }),
   },
 
   creator: {
+    list: () => fetcher({ url: '/creator', method: 'GET' }),
     find: (name) => fetcher({ url: `/creator?name=${name}`, method: 'GET' }),
   },
 
