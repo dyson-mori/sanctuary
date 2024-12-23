4f85c6706b846f0a9e0fa26ffc327970cab35904
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2024-12-23 18:02:19 -0300

    :hammer_and_wrench: remove

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,7 +1,7 @@
 // import 'styled-components';
 // import theme from '../global/theme';
 
-// type ThemeProps = typeof theme;
+// type ThemeProps = typeof theme.light;
 
 // declare module 'styled-components' {
 //   export interface DefaultTheme extends ThemeProps { }
